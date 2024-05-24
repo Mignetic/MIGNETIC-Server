c8ef268fd978be93b688cde8bf461e3a7067e207
hxxjx523 <hhj05230@gmail.com>
2024-05-24 15:06:04 +0900

    게시판 수정

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,5 +1,5 @@
 import Logo from '../images/icons/logo.png'
-import bgImg2 from '../images/noticeBoard-bg2.png'
+import letterImg from '../images/icons/letterImg.png'
 
 import '../css/NoticeBoard.css'
 
@@ -9,7 +9,48 @@ function NoticeBoard() {
     return (
         <div class="noticeBoard-background">
             <div class="logo-container"><img src={Logo} class=".boardLogo"/></div>
-            <div class="boardBg"></div>
+            <div class="boardLetter">
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'><p className='leftP'>To.승주</p><p className='rightP'>from.승주</p></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'>To.윤서</div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+                <div className='boardLetterImg'>
+                    <div className='letterFront'><img src={letterImg}/></div>
+                    <div className='letterBack'></div>
+                </div>
+
+
+
+
+            </div>
             
         </div>        
     )
