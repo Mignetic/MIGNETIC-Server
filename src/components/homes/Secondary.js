4a7dc21b38ae09f419cdbe8a12cf7439c5d37aa5
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-05-26 03:29:39 +0900

    Home구현중(Last.js 완료)

---
+++ src/components/homes/Secondary.js
@@ -0,0 +1,11 @@
+import bgImg from '../../images/secondary-bg.png'
+
+function Secondary() {
+    return (
+        <div className="Secondary" style={{ backgroundImage: `url(${bgImg})` }}>
+
+        </div>
+    )
+}
+
+export default Secondary
