8db02bd7c523876b96a30f9935b8dc6a15c08bfc
didwldk <118715150+didwldk@users.noreply.github.com>
2024-05-25 17:30:03 +0900

    Result 개발 시작

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -3,4 +3,11 @@ import bgImg from '../images/result-bg.png'
 function Result (){
     document.body.style.backgroundImage = `url(${bgImg})`
 
+    return(
+        <div className='result'>
+
+        </div>
+    )
 }
+
+export default Result;
