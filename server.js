81202ff15c53f16d88d8759b8db7b19e8832bf3b
yoonseo232 <ys.bang232@gmail.com>
2024-06-11 09:09:42 +0900

    build 생성

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
 const app = express();
 const port = 8888;
 
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: '[유저이름]',
-    password: '[비밀번호]',
-    database: '[데이터베이스 이름]'
-});
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: '[유저이름]',
+//     password: '[비밀번호]',
+//     database: '[데이터베이스 이름]'
+// });
 
-db.connect((err) => {
-    if (err) {
-        console.error('에러 :', err);
-        return;
-    }
-    console.log('MySQL에 연결');
-});
+// db.connect((err) => {
+//     if (err) {
+//         console.error('에러 :', err);
+//         return;
+//     }
+//     console.log('MySQL에 연결');
+// });
 
-app.get('/api/[테이블 이름]', (req, res) => {
-    const sql = 'SELECT * FROM [테이블 이름]';
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error('에러 :', err);
-            res.status(500).json({ error: 'Database query error' });
-            return;
-        }
-        res.json(results);
-    });
+// app.get('/api/[테이블 이름]', (req, res) => {
+//     const sql = 'SELECT * FROM [테이블 이름]';
+//     db.query(sql, (err, results) => {
+//         if (err) {
+//             console.error('에러 :', err);
+//             res.status(500).json({ error: 'Database query error' });
+//             return;
+//         }
+//         res.json(results);
+//     });
+// });
+
+app.get("/", (req, res) => {
+    res.send("hi");
 });
 
 app.listen(port, () => {
