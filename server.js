e8bb3305297f7c42133cd828c8fd07cf059a71cb
seoungju0828 <ministar2006@gmail.com>
2024-06-11 13:45:50 +0900

    DB 연결

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
-const express = require('express');
+require('dotenv').config();
 const mysql = require('mysql');
-const cors = require('cors');
-const app = express();
-const port = 8888;
 
-app.use(cors());
-
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: '[유저이름]',
-//     password: '[비밀번호]',
-//     database: '[데이터베이스 이름]'
-// });
-
-// db.connect((err) => {
-//     if (err) {
-//         console.error('에러 :', err);
-//         return;
-//     }
-//     console.log('MySQL에 연결');
-// });
-
-// app.get('/api/[테이블 이름]', (req, res) => {
-//     const sql = 'SELECT * FROM [테이블 이름]';
-//     db.query(sql, (err, results) => {
-//         if (err) {
-//             console.error('에러 :', err);
-//             res.status(500).json({ error: 'Database query error' });
-//             return;
-//         }
-//         res.json(results);
-//     });
-// });
-
-app.get("/", (req, res) => {
-    res.send("hi");
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '111111',
+    database: process.env.DB_DATABASE || 'mignetic',
+    port: process.env.DB_PORT || 3307
 });
 
-app.listen(port, () => {
-    console.log(`포트 -> ${port}`);
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as id', connection.threadId);
+});
+
+connection.query('SELECT 1 + 1 AS solution', (err, results, fields) => {
+    if (err) {
+        console.error('Error executing query:', err.stack);
+        connection.end();
+        return;
+    }
+    console.log('The solution is: ', results[0].solution);
+    connection.end((err) => {
+        if (err) {
+            console.error('Error ending the connection:', err.stack);
+        } else {
+            console.log('Connection closed');
+        }
+    });
 });
